4edf308a98d10df478ba6322091e9830d84d2a8f
ste2021 <estela.oliveira.cont@outlook.com>
2020-08-05 21:07:55 -0300

    Shaping React API with axios

---
+++ instructions.sh
@@ -0,0 +1,14 @@
+
+Para comitar
+
+echo "# teste" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+
+primeira vez
+git remote add origin https://github.com/ste2021/teste.git
+
+
+dps q fazer alterações.
+git push -u origin master
